af75298802ba4f1cf391d735aaebabb4b14704ad
StellenDasStahlUberall <brantheif115@gmail.com>
2016-12-19 23:27:21 -0500

    Changed bk_mod.java to have actual mod name

--- src/main/java/com/brandonkyle/bk_mod/bk_mod.java
+++ src/main/java/com/brandonkyle/bk_mod/bk_mod.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package com.brandonkyle.bk_mod;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BK_Mod.MODID, version = alpha_1.VERSION)
+public class BK_Mod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "BK_Mod";
+    public static final String VERSION = "alpha_1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
